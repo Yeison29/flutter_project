4c4052dcd85d16a901eabec3398408ddd2b44166
Yeison Ascanio <ydascanioa@ufpso.edu.co>
2023-03-30 05:41:25 +0200

    Se creo la interfaz de registrar

--- lib/src/register/register_page.dart
+++ lib/src/register/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:delivery/src/utils/my_colors.dart';
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
@@ -9,6 +10,236 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      
+      body: Container(
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Positioned(
+            child: _imageUser(),
+            left: 150,
+            top: 15,
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 135),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    _textFieldEmail(),
+                    _textFieldName(),
+                    _textFieldLastName(),
+                    _textFieldPhone(),
+                    _textFieldPassword(),
+                    _textFieldConfirmPassword(),
+                    _buttonRegister()
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+            child: _circleRegister(),
+            left: -110,
+            top: -100,),
+            Positioned(
+            child: _textRegister(),
+            left: 25,
+            top: 52,),
+            Positioned(
+            child: _iconBack(),
+            left: -4,
+            top: 35,)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _imageUser() {
+    return CircleAvatar(
+      backgroundImage: AssetImage('assets/img/user_profile_2.png'),
+      radius: 60,
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+   Widget _iconBack() {
+    return IconButton(
+      onPressed: () {},
+      icon: Icon(
+        Icons.arrow_back_ios,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget _circleRegister() {
+    return Container(
+      width: 240,
+      height: 240,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(90),
+          color: MyColors.primaryColor),
+    );
+  }
+Widget _textRegister() {
+    return Text(
+      'REGISTER',
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 15,
+        fontFamily: 'NimbusSans',
+      ),
+    );
+  }
+
+  Widget _textFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 5),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Correo electronico",
+          hintStyle: TextStyle(color: MyColors.primaryColorDart),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(
+            Icons.email,
+            color: MyColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldName() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 5),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Nombre",
+          hintStyle: TextStyle(color: MyColors.primaryColorDart),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(
+            Icons.person,
+            color: MyColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldLastName() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 5),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Apellido",
+          hintStyle: TextStyle(color: MyColors.primaryColorDart),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(
+            Icons.person_outline,
+            color: MyColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldPhone() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 5),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Telefono",
+          hintStyle: TextStyle(color: MyColors.primaryColorDart),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(
+            Icons.phone,
+            color: MyColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 5),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Contraseña",
+          hintStyle: TextStyle(color: MyColors.primaryColorDart),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(
+            Icons.lock,
+            color: MyColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _textFieldConfirmPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 5),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "Confirmar Contraseña",
+          hintStyle: TextStyle(color: MyColors.primaryColorDart),
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(
+            Icons.lock_outline,
+            color: MyColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonRegister() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('REGISTRARSE'),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: MyColors.primaryColor,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            padding: const EdgeInsets.symmetric(vertical: 15)),
+      ),
+    );
   }
 }
